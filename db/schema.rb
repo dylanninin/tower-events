427cdf2bd0396b32bb3281687e3c21baab8eabff
Dylan <dylanninin@gmail.com>
2017-06-02 14:13:54 +0800

    Ref #3 Update db schema
    Sucks...

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,4 @@ ActiveRecord::Schema.define(version: 20170602044144) do
     t.index ["deleted_at"], name: "index_dummies_on_deleted_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
