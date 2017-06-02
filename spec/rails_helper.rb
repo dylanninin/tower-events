2e3f009237e5a9e820f4ff9b900ecbd4e6139960
Dylan <dylanninin@gmail.com>
2017-06-02 14:12:34 +0800

    Ref #3 Integrate dotenv

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
 # FIXME: For runing testing on `test` environment
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+# AutoLoad .env files: .env, .env.{environment}, .env.local
+Dotenv::Railtie.load
+
+require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
