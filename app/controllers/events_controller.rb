07d24a97670e368244d48cdfb30120d79430af51
Dylan <dylanninin@gmail.com>
2017-06-04 14:18:37 +0800

    Ref #9 Initial events views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,7 @@
+class EventsController < ApplicationController
+  # GET /events
+  # GET /events.json
+  def index
+    @events = Event.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   resources :accesses
   resources :comments
   resources :todos
