4914af5f4a9b2f8c1095b14f7b35b16148f859ed
Dylan <dylanninin@gmail.com>
2017-06-04 21:13:46 +0800

    Ref #9 Add pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.order('published desc')
+    @events = Event.order('published desc').page(params[:page]).per(50)
     @event_groups = @events.group_by { |event| event.published.to_date }
   end
 end
