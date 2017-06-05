e7bccdd5220c3c70718b4feadabba49ba5295ccc
Dylan <dylanninin@gmail.com>
2017-06-05 09:26:09 +0800

    Fix json to jsonb error on travis-ci https://travis-ci.org/dylanninin/tower-events/builds/239363174

--- db/migrate/20170604041011_alter_events_columns.rb
+++ db/migrate/20170604041011_alter_events_columns.rb
@@ -1,9 +1,37 @@
 class AlterEventsColumns < ActiveRecord::Migration[5.1]
   def change
-    change_column :events, :actor, :jsonb, null: false
-    change_column :events, :object, :jsonb, null: false
-    change_column :events, :target, :jsonb
-    change_column :events, :provider, :jsonb
-    change_column :events, :generator, :jsonb
+    change_column_null :events, :actor, false
+    change_column_null :events, :object, false
+
+    # FIXME Postgres migration error on travis-ci
+    # error:
+    # => PG::DatatypeMismatch: ERROR:  column "actor" cannot be cast automatically to type jsonb
+    # => HINT:  Specify a USING expression to perform the conversion.
+    # => : ALTER TABLE "events" ALTER COLUMN "actor" TYPE jsonb
+    # https://travis-ci.org/dylanninin/tower-events/builds/239363174
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          alter table "events"
+          alter column "actor" type jsonb using actor::jsonb,
+          alter column "object" type jsonb using object::jsonb,
+          alter column "target" type jsonb using target::jsonb,
+          alter column "provider" type jsonb using provider::jsonb,
+          alter column "generator" type jsonb using generator::jsonb
+        SQL
+      end
+
+      dir.down do
+        execute <<-SQL
+          alter table "events"
+          alter column "actor" type json using actor::json,
+          alter column "object" type json using object::json,
+          alter column "target" type json using target::json,
+          alter column "provider" type json using provider::json,
+          alter column "generator" type json using generator::json
+        SQL
+      end
+    end
   end
+
 end
