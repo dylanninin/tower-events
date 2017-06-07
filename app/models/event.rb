1244b7b6c77d5fa06028a1d7f33d613d58dfcbbc
Dylan <dylanninin@gmail.com>
2017-06-07 17:54:44 +0800

    Ref #15 Do creating events directly called by callbacks via models

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,6 +20,42 @@ class Event < ApplicationRecord
       ws.inject(all, :merge)
     end
 
+    # Audit object attribute if it has been changed
+    # object: the current object being audited
+    #
+    # opts:
+    # name: required. the attribute to be audit
+    # alias: optional. the attribute alias name
+    # old_value?: optional. proc to check if the old value has satisfied the condition
+    # new_value?: optional. proc to check if the new value has satisfied the condition
+    # value_proc: optional. proc to re-evaluate the new value after it has satisfied the condition
+    def object_attribute_audited(object, opts = {})
+      return nil unless opts.present?
+
+      attr_name = opts[:name]
+      return nil unless attr_name.present?
+
+      change = object.send(:"#{attr_name}_change")
+      return nil unless change.present?
+
+      need_audit = true
+      %i(old_value? new_value?).each_with_index do |v, i|
+        c = opts[v]
+        next if c.blank?
+        unless c.call(change[i])
+          need_audit = false
+          break
+        end
+      end
+      return nil unless need_audit
+
+      {
+        :attribute => opts[:alias] || attr_name,
+        :old_value => opts[:value_proc] ? opts[:value_proc].call(change[0])&.as_partial_event : change[0],
+        :new_value => opts[:value_proc] ? opts[:value_proc].call(change[1])&.as_partial_event : change[1]
+      }
+    end
+
     # Create event
     def create_event(opts = {})
       object = opts[:object]
@@ -27,7 +63,12 @@ class Event < ApplicationRecord
       event.actor = event_partial(object, :actor, opts, event_default_actor)
       event.verb = opts[:verb]
       event.object = object&.as_partial_event
-      event.object[:audited] = opts[:audited] if opts[:audited].present? && event.object.present?
+      if opts[:changed_attribute].present?
+        audited = object_attribute_audited(object, opts[:changed_attribute])
+      else
+        audited = opts[:audited]
+      end
+      event.object[:audited] = audited if audited.present? && event.object.present?
       event.target = event_partial(object, :target, opts)
       event.generator = event_partial(object, :generator, opts)
       event.provider = event_partial(object, :provider, opts)
@@ -48,53 +89,6 @@ class Event < ApplicationRecord
     def event_default_actor
       User.current
     end
-  end
 
-  # TODO: Title can be removed
-  # For flexibility, title has not been persisted
-  def title
-    words = []
-    words << actor['name']
-    case verb
-    when 'create', 'destroy', 'run', 'pause', 'complete', 'reopen', 'recover'
-      words << verb
-      words << object['type']
-      words << ':'
-      words << object['name']
-    when 'set_due_to'
-      words << verb
-      words << "from #{object['audited']['old_value']}"
-      words << "to #{object['audited']['new_value']}"
-      words << ':'
-      words << object['name']
-    when 'assign'
-      words << verb
-      words << target['name']
-      words << ":"
-      words << object['name']
-    when 'reassign'
-      words << object['audited']['old_value']['name']
-      words << object['type']
-      words << 'to'
-      words << object['audited']['new_value']['name']
-      words << ':'
-      words << object['name']
-    when 'reply'
-      words << verb
-      words << target['type']
-      words << ':'
-      words << target['name']
-    end
-    words.join(' ')
   end
-
-  # TODO: Content can be removed
-  # For flexibility, content has not been persisted
-  def content
-    case verb
-    when 'reply'
-      object['text']
-    end
-  end
-
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,17 +1,6 @@
 class Todo < ApplicationRecord
   include Eventable
   eventablize_serializer_attrs :name
-  eventablize_ops_context :create
-  eventablize_ops_context :destroy
-  # FIXME: For consistency, set_due rename to set_due_to
-  eventablize_ops_context :update, verb: :set_due_to, attr: :due_to
-  eventablize_ops_context :update, verb: :assign, target: :assignee, attr: :assignee_id, attr_alias: :assignee, value_proc: -> (v) { User.where(id: v).first }, old_value?: -> (v) { v.nil? }, new_value?: -> (v) { v.present? }
-  eventablize_ops_context :update, verb: :reassign, target: :assignee, attr: :assignee_id, attr_alias: :assignee, value_proc: -> (v) { User.where(id: v).first }, old_value?: -> (v) { v.present? }, new_value?: -> (v) { v.present? }
-  eventablize_ops_context :update, verb: :run, attr: :status, new_value?: -> (v) { v == 'running' }
-  eventablize_ops_context :update, verb: :pause, attr: :status, new_value?: -> (v) { v == 'paused' }
-  eventablize_ops_context :update, verb: :complete, attr: :status, new_value?: -> (v) { v == 'completed' }
-  eventablize_ops_context :update, verb: :reopen, attr: :status, old_value?: -> (v) { v == 'completed' },  new_value?: -> (v) { v == 'open' }
-  eventablize_ops_context :update, verb: :recover, attr: :deleted_at, old_value?: -> (v) { v.present? },  new_value?: -> (v) { v.nil? }
 
   enum status: { open: 0, running: 1, paused: 2, completed: 3 }
 
@@ -20,6 +9,106 @@ class Todo < ApplicationRecord
   belongs_to :project
   belongs_to :team
   belongs_to :creator, class_name: 'User'
+
+  after_create_commit :add_event_after_create_todo
+  def add_event_after_create_todo
+    Event.create_event(verb: :create, object: self)
+  end
+
+  around_update :add_event_after_assign_todo
+  def add_event_after_assign_todo
+    yield
+    changed_attribute = {
+      name: :assignee_id,
+      alias: :assignee,
+      old_value?: -> (v) { v.nil? },
+      value_proc: -> (v) { User.where(id: v).first }
+    }
+    Event.create_event(verb: :assign, object: self, target: :assignee, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_reassign_todo
+  def add_event_after_reassign_todo
+    yield
+    changed_attribute = {
+      name: :assignee_id,
+      alias: :assignee,
+      old_value?: -> (v) { v.present? },
+      new_value?: -> (v) { v.present? },
+      value_proc: -> (v) { User.where(id: v).first }
+    }
+    Event.create_event(verb: :reassign, object: self, target: :assignee, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_set_due_to_todo
+  def add_event_after_set_due_to_todo
+    yield
+    changed_attribute = {
+      name: :due_to
+    }
+    Event.create_event(verb: :set_due_to, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_run_todo
+  def add_event_after_run_todo
+    yield
+    changed_attribute = {
+      name: :status,
+      new_value?: -> (v) { v == 'running' }
+    }
+    Event.create_event(verb: :run, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_pause_todo
+  def add_event_after_pause_todo
+    yield
+    changed_attribute = {
+      name: :status,
+      new_value?: -> (v) { v == 'paused' }
+    }
+    Event.create_event(verb: :pause, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_complete_todo
+  def add_event_after_complete_todo
+    yield
+    changed_attribute = {
+      name: :status,
+      new_value?: -> (v) { v == 'complete' }
+    }
+    Event.create_event(verb: :complete, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_reopen_todo
+  def add_event_after_reopen_todo
+    yield
+    changed_attribute = {
+      name: :status,
+      new_value?: -> (v) { v == 'open' }
+    }
+    Event.create_event(verb: :reopen, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_recover_todo
+  def add_event_after_recover_todo
+    yield
+    changed_attribute = {
+      name: :deleted_at,
+      new_value?: -> (v) { v.nil? }
+    }
+    Event.create_event(verb: :recover, object: self, changed_attribute: changed_attribute)
+  end
+
+  around_update :add_event_after_destroy_todo
+  def add_event_after_destroy_todo
+    yield
+    changed_attribute = {
+      name: :deleted_at,
+      old_value?: -> (v) { v.nil? },
+      new_value?: -> (v) { v.present? }
+    }
+    Event.create_event(verb: :destroy, object: self, changed_attribute: changed_attribute)
+  end
 
   # Default provider for all events
   def eventablize_provider
